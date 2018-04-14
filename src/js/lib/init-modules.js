85e697ff9f90eccdc00e161cf3e60a4d51b6724e
Wes Turner <wturnerharris@gmail.com>
2018-04-14 15:25:16 -0400

    Update init module pattern

--- src/js/lib/init-modules.js
+++ src/js/lib/init-modules.js
@@ -1,31 +1,68 @@
-/* globals document */
+import { tryCatch, identity, ifElse, getProp, pipe, partial } from 'lib/utils'
+import { selectAll, getData } from 'lib/dom'
+import 'babel-polyfill'
+
+/**
+ * Require a module by name
+ *
+ * @param {string} name
+ * @return {Function|Object}
+ */
+const requireModule = (name) => require('modules/' + name)
+
+/**
+ * Handle exception
+ *
+ * @param {Error} err
+ * @returns {boolean}
+ */
+const handleError = (err) => {
+  console.log(err.toString())
+  return false
+}
+
+/**
+ * Check to see if a module is an ES6 wrapper
+ *
+ * @param {Function|Object} val
+ * @return {boolean}
+ */
+const isEs6Module = (val) => typeof val === 'object' && typeof val.default !== 'undefined'
+
+/**
+ * Create new module instance for element
+ *
+ * @param {HTMLElement} el
+ * @param {Function} Module
+ * @returns {Module}}
+ */
+const runModule = (el, Module) => {
+	if (Module) {
+		new Module(el)
+	} else {
+		console.log(el)
+	}
+}
+
+/**
+ * Initialize module based on attribute name
+ *
+ * @param {HTMLElement} el
+ * @returns {HTMLElement|Module}
+ */
+const initializeModuleForEl = (el) => {
+  return pipe(
+    getData('module-init'),
+    tryCatch(requireModule, handleError),
+    ifElse(isEs6Module, getProp('default'), identity),
+    partial(runModule, el)
+  )(el)
+}
+
 /**
  * Finds all elements with a "data-module-init" attribute
  * and calls the corresponding script
+ *
+ * @return {Array<HTMLElement|Object>}
  */
-function initializeModules() {
-  var modules = document.querySelectorAll( "[data-module-init]" );
-
-  for ( var i = 0; i < modules.length; i++ ) {
-    var el = modules[ i ];
-    var names = el.getAttribute( "data-module-init" ).split( " " );
-    var Module;
-
-    for ( var j = 0; j < names.length; j++ ) {
-      // Find the module script
-      try {
-        Module = require( "modules/" + names[ j ] );
-      } catch ( e ) {
-        Module = false;
-        console.log( names[ j ] + " module does not exist." );
-      }
-
-      // Initialize the module with the calling element
-      if ( Module ) {
-        new Module( el );
-      }
-    }
-  }
-}
-
-module.exports = initializeModules;
+export default () => selectAll('[data-module-init]').map(initializeModuleForEl)
